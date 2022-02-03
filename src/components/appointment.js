10b94d80b587b6da2782497caed61cf4e1eff9ec
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-02-03 14:51:10 -0300

    initial commit

---
+++ src/components/appointment.js
@@ -0,0 +1,15 @@
+const dumbData = {
+  customer: { 
+    firstName: 'Ashley',
+    lastName: 'Jones',
+    phoneNumber: '(123)555-0123'
+  },
+  stylist: 'Jay Speares',
+  startsAt: '2019-02-02 09:30',
+  service: 'Cut',
+  notes: ''
+}
+
+export default function Appointment(){
+  return (null)
+}
