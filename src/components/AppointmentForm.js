e764ec03b73cbb548f71e650bbd2f82de086d75e
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-03-27 21:19:50 -0300

    init appointment form

---
+++ src/components/AppointmentForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function AppointmentForm() {
+  return (
+    <form action="" id="appointment">
+      <select name="service" id=""></select>
+    </form>
+  )
+}
---
+++ src/test/AppointmentForm.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createContainer } from './domManipulator';
+import { AppointmentForm } from '../components/AppointmentForm';
+
+describe('AppointmentForm', () => {
+  let render, container
+
+  beforeEach(() => {
+    ({render, container} = createContainer())
+  })
+
+  const form = id => container.querySelector(`form[id="${id}"]`)
+  const field = name => form('appointment').elements[name]
+
+  it('renders a form', () => {
+    render(<AppointmentForm />)
+
+    expect(form('appointment')).not.toBeNull()
+  })
+
+  describe('service field', () => {
+    it('renders as a select box', () => {
+      render(<AppointmentForm />)
+
+      expect(field('service')).not.toBeNull()
+      expect(field('service').tagName).toEqual('SELECT')
+    })
+  })
+})
